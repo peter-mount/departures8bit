65f500668ad169faa1d66e842cdcb55fa8b07e18
peter <peter@retep.org>
2021-04-02 14:42:47 +0100

    New bootloader completed

--- apps/c64/bootstrap.asm
+++ apps/c64/bootstrap.asm
@@ -8,61 +8,102 @@
     INCLUDE "kernal.asm"            ; Kernal definitions
     INCLUDE "teletext.inc"          ; Teletext entry points
 
-tempAddr = &00FB                ; Temp address
+fileCount   = &00FB             ; Current file number
+fileName    = &00FC             ; Current filename
+writeStr    = &00FE             ; text routine to use
 
 start = &7000                   ; Base of bootstrap
     ORG start-2                 ; Start 2 bytes earlier so we can inject the
     EQUW start                  ; load address for the prg file format
-
+{
     LDA #%00110110              ; Replace basic with ram at A000-BFFF for an extra 8K
     STA &01
 
-    JSR writeKernalBanner       ; Write initial banner to standard screen
+    LDA #0                      ; First file
+    STA fileCount
 
-    LDXY teletext               ; Load teletext driver
-    JSR loadFileKernal          ; Kernal file loader until we have teletext loaded
+    LDA #<files                 ; Point to first filename
+    STA fileName
+    LDA #>files
+    STA fileName+1
+
+    LDA #<writeKernal           ; Use kernal to write text initially
+    STA writeStr
+    LDA #>writeKernal
+    STA writeStr+1
+
+.L1 LDY #0                      ; Check for file list terminator
+    LDA (fileName),Y
+    BEQ L4                      ; All done
+
+    JSR showFilename            ; Display "Loading... file" message
+
+    JSR loadFile                ; Load the current file
+    CPY #&08                    ; If last byte loaded was below &0800 then
+    BPL L2                      ; refresh the screen as we just loaded a splash
+    CPY #&00                    ; ensure Y is < 800 ignore &C00 which can still
+    BMI L2                      ; trigger the refresh
+    JSR refreshScreen           ; page
+.L2
+
+    LDA fileCount               ; Check if we have loaded the first file
+    BNE L3                      ; if not then skip teletext initialisation
+
     JSR teletextInit            ; Initialise teletext emulator
     JSR writeTeletextBanner     ; Write teletext banner
 
-;    JSR refreshScreen           ; Refresh to show the loaded splash page
-;.LL JMP LL                      ; Debug lock on splash screen
-.LL
-    LDXY splash                 ; Load splash page direct to teletext screen
-    JSR loadFile
-;.LL
-    JSR refreshScreen           ; Refresh to show the loaded splash page
+    LDA #<writeTele             ; Switch to teletext code to write loading text
+    STA writeStr
+    LDA #>writeTele
+    STA writeStr+1
 
-                                ; Uncomment this to hold on splash screen when
-;.LL
-;   JMP LL                      ; Debug lock on splash screen
+.L3 INC fileCount               ; Mark not the first file
+    JSR strLen                  ; Get filename length
+    INY                         ; Include null terminator
+    TYA
+    CLC                         ; Add it to FileName
+    ADC fileName
+    STA fileName
+    LDA fileName+1
+    ADC #0
+    STA fileName+1
+    BNE L1                      ; BRA to L1
 
-    LDXY banner                 ; Load splash page direct to teletext screen
-    JSR loadFile
-    JSR refreshScreen           ; Refresh to show the loaded splash page
+.L4 JMP &0900                   ; Run the application
+}
 
-                                ; Uncomment this to hold on splash screen when
-;.LL JMP LL                      ; Debug lock on splash screen
-;    JMP LL
+; As there's no JSR (writeStr)
+.showFilename JMP (writeStr)
 
-    LDXY app                    ; Load the application
-    JSR loadFile
-
-    JMP &0900                   ; Run the application
-
-.loadFile
+.writeKernal                    ; Write Loading before teletext loaded
 {
-    STXY tempAddr               ; Store filename address
+    LDY #0                      ; Print "LOADING" to Kernal screen
+.L1 LDA banner,Y                ; Standard send text to CHROUT until we hit 0
+    BEQ L2
+    JSR CHROUT
+    INY
+    BNE L1
+.L2 LDY #0                      ; Print filename
+.L3 LDA (fileName),Y
+    BEQ L4
+    JSR CHROUT
+    INY
+    BNE L3
+.L4 RTS
+.banner
+    EQUS "LOADING ", 0
+}
+
+.writeTele                      ; Write loading on teletext screen
+{
     LDX #<TX                    ; Move cursor to 21,0 & set white text
     LDY #>TX
     JSR writeString
-    JSR L0                      ; write filename with padding
-    JMP LF                      ; Load the file
-
+                                ; Then the filename, padding to EOL
 .L0 LDX #40-22-8                ; Max chars to write 8=len("Loading ")
     LDY #0
-.L1 LDA (tempAddr),Y            ; Next char
+.L1 LDA (fileName),Y            ; Next char
     BEQ L2                      ; End of string
-
     JSR oswrch                  ; Write char
     INY
     DEX
@@ -74,33 +115,31 @@ start = &7000                   ; Base of bootstrap
     BNE L3
     RTS
 .TX EQUS 31,21,0,135,"Loading ",0          ; TAB(21,0), WhiteText
-.TE EQUB 31,0,1,0               ; TAB(0,1)
 }
 
-.loadFileKernal
-    STXY tempAddr               ; Store filename address
-.LF LDA #8                      ; Logical file number
+.loadFile
+    LDA #8                      ; Logical file number
     LDX #8                      ; Device 8 disk
     LDY #1                      ; Load with address in file
     JSR SETLFS
 
-    JSR strlen                  ; Get filename length
-    LDX tempAddr                ; SETNAM on filename
-    LDY tempAddr+1
+    JSR strLen                  ; Get filename length
+    TYA                         ; into A for SETNAM
+    LDX fileName                ; SETNAM on filename
+    LDY fileName+1
     JSR SETNAM
 
     LDA #0                      ; Flag LOAD
     JMP LOAD                    ; Load into memory
 
-.strlen
+.strLen                         ; Length of (fileName) excluding terminator in Y
 {
     LDY #0
-.L1 LDA (tempAddr),Y
+.L1 LDA (fileName),Y
     BEQ L2
     INY
     BNE L1
-.L2 TYA
-    RTS
+.L2 RTS
 }
 
 ; C64 standard screen message
@@ -124,16 +163,19 @@ start = &7000                   ; Base of bootstrap
     LDY #>banner
     JMP writeString
 .banner
-    EQUS 132, "Area51 Teletext", 135, "C64", 129, "1.0", 13, 10, 10
-    EQUS 130, "Loading application...", 13, 10
-    EQUB 0
+    EQUS 30, 134, "departureboards.mobi", 13, 10, 10
+    EQUS 132, 157, 135, 141, "      Live UK Departure Boards      "
+    EQUS 132, 157, 135, 141, "      Live UK Departure Boards      "
+    EQUS 10, 130, "Please wait whilst loading completes...", 0
 }
 
-.teletext   EQUS "TELETEXT", 0
-.splash     EQUS "SPLASH", 0
-.banner     EQUS "BANNER", 0
-.app        EQUS "DEPART", 0
-
+; List of files to load, terminated with 0
+.files
+    EQUS "TELETEXT", 0          ; TELETEXT must be the first file to load
+    EQUS "SPLASH", 0
+    EQUS "BANNER", 0
+    EQUS "DEPART", 0
+    EQUB 0                      ; list terminator
 .end
     ; Save the program, start-2 to include the start address
     SAVE "bootstrap", start-2, end
