bf59dacf7148db4af5d0ef4a73ba9e618dd25852
peter <peter@retep.org>
2021-04-05 21:19:40 +0100

    Restore bootstrap to run main app

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This project contains the various "apps" to compliment the react site on departureboards.mobi.
 
+This is a work in progress, so don't expect much to work just yet.
--- c64/bootstrap.asm
+++ c64/bootstrap.asm
@@ -60,8 +60,8 @@ start = &7000                   ; Base of bootstrap
     STA fileName+1
     BNE L1                      ; BRA to L1
 
-.L4 JMP L4
-;.L4 JMP &0900                   ; Run the application
+;.L4 JMP L4
+.L4 JMP &0900                   ; Run the application
 }
 
 .showFilename
@@ -148,10 +148,10 @@ start = &7000                   ; Base of bootstrap
 ; List of files to load, terminated with 0
 .files
     EQUS "TELETEXT", 0          ; TELETEXT must be the first file to load
-    EQUS "TESTCARD", 0          ; Teletext test card for debugging
-    ;EQUS "SPLASH", 0            ; Our splash page
-    ;EQUS "NETWORK", 0           ; Network driver
-    ;EQUS "BOARDS", 0            ; The main application
+    ;EQUS "TESTCARD", 0          ; Teletext test card for debugging
+    EQUS "SPLASH", 0            ; Our splash page
+    EQUS "NETWORK", 0           ; Network driver
+    EQUS "BOARDS", 0            ; The main application
     EQUB 0                      ; list terminator
 .end
     ; Save the program, start-2 to include the start address
